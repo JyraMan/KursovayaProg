040f03d9f66df919208edb7d5374a1152eba58bf
JyraMan <tgazievr@mail.ru>
2023-05-25 18:31:48 +0700

    update

--- bitio.h
+++ bitio.h
@@ -16,12 +16,12 @@ typedef struct {
 	int rack;
 } BITFILE;
 
-BITFILE* OpenBitFileInput(char* testin);
-BITFILE* OpenBitFileOutput(char* testout);
-void CloseBitFileInput(BITFILE* bf);
-void CloseBitFileOutput(BITFILE* bf);
-int BitInput(BITFILE* bf);
-unsigned long BitsInput(BITFILE* bf, int count);
-void BitOutput(BITFILE* bf, int bit);
-void BitsOutput(BITFILE* bf, unsigned long code, int count);
+BITFILE OpenBitFileInput(char* testin);
+BITFILE OpenBitFileOutput(char* testout);
+void CloseBitFileInput(BITFILE bf);
+void CloseBitFileOutput(BITFILE bf);
+int BitInput(BITFILE bf);
+unsigned long BitsInput(BITFILE bf, int count);
+void BitOutput(BITFILE bf, int bit);
+void BitsOutput(BITFILE bf, unsigned long code, int count);
 #endif	// __BITIO__
